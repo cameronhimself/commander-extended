c79ba5dcd79547c420f8002fe88982971cc98fec
J. Cameron McDonald <cameronhhimself@gmail.com>
2025-05-10 22:38:28 -0500

    exclude tests from build

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,6 @@
 import typescript from "@rollup/plugin-typescript";
 import json from "@rollup/plugin-json";
 
-
 export default {
   input: "src/index.ts",
   output: {
@@ -10,7 +9,9 @@ export default {
   external: ["commander"],
   plugins: [
     json(),
-    typescript(),
+    typescript({
+      exclude: ["**/*.test.ts", "**/test/**"],
+    }),
   ],
 };
 
